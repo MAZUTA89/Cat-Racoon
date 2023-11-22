bb0a9a434a45cb95fc70eba7e59f8bdaee673e85
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2023-11-22 21:10:16 +0300

    add client-server code to unity project

---
+++ FarmVille/Assets/Code/ClientServer/PrefixWriterReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace ClientServer
+{
+    public class PrefixWriterReader
+    {
+        public int PrefixSize { get; private set; }
+        public int PrefixLength { get; private set; }
+        const string c_prefixBase = "Size:[ ]";
+        const string c_sizeGroupName = "Size";
+        const string c_sizePattern = @"Size:\[(?'Size'\d+)\s*\]";
+        string _prefix;
+        StringBuilder _prefixBuilder;
+        Regex _sizeRegex;
+        public PrefixWriterReader(int prefixSize)
+        {
+            _prefixBuilder = new StringBuilder();
+            _sizeRegex = new Regex(c_sizePattern);
+            PrefixSize = prefixSize;
+        }
+        public void InitializePrefix(int byteLength)
+        {
+            _prefixBuilder.Clear();
+            _prefixBuilder.Append(c_prefixBase);
+            _prefix = byteLength.ToString();
+            _prefixBuilder = _prefixBuilder.Replace(" ", _prefix);
+        }
+        
+        public string WriteInfoPrefix(string local)
+        {
+            _prefixBuilder.Append(local);
+            return _prefixBuilder.ToString();
+        }
+        public int ReadInfoPrefix(string local)
+        {
+            if (_sizeRegex.IsMatch(local))
+            {
+                Match match = _sizeRegex.Match(local);
+                var sizeData = match.Value;
+                PrefixLength = sizeData.Length;
+                return int.Parse(match.Groups[c_sizeGroupName].Value);
+            }
+            else
+            {
+                throw new Exception("Prefix massage failed to read");
+            }
+        }
+    }
+}
---
+++ FarmVille/Assets/Code/ClientServer/Server/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer.Server
+{
+    public class Server : TCPMessenger
+    {
+        const int c_backlog = 1;
+        EndPoint _clientEndPoint;
+        Socket _serverSocket;
+        public Server()
+        {
+            EndPoint = new IPEndPoint(IPAddress.Any, 9000);
+            _serverSocket = base.InitializeTCPSocket();
+        }
+
+        public bool TryBindPoint()
+        {
+            try
+            {
+                _serverSocket.Bind(EndPoint);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _error = ex;
+                return false;
+            }
+        }
+        public bool Listen()
+        {
+            try
+            {
+                _serverSocket.Listen(c_backlog);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                _error = ex;
+                return false;
+            }
+        }
+
+        public async Task TryAcceptAsync()
+        {
+            try
+            {
+                ClientSocket = await _serverSocket.AcceptAsync();
+                RemoteEndPoint = ClientSocket.RemoteEndPoint;
+                LocalEndPoint = ClientSocket.LocalEndPoint;
+                //return true;
+            }
+            catch (Exception ex)
+            {
+                _error = ex;
+                //return false;
+            }
+        }
+
+        public override bool Stop()
+        {
+            return StopSocket(ClientSocket) && StopSocket(_serverSocket);
+        }
+    }
+}
