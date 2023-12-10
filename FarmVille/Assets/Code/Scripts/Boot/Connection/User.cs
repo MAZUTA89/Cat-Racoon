ecb1158bf392352a2486c88a36a327e2204b64b9
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2023-12-10 22:10:47 +0300

    00005

--- FarmVille/Assets/Code/Scripts/Boot/Connection/User.cs
+++ FarmVille/Assets/Code/Scripts/Boot/Connection/User.cs
@@ -56,7 +56,7 @@ namespace Assets.Code.Scripts.Boot
         public async void OnLevelLoaded()
         {
             _communicator = new Communicator
-                (_userBase, 005);
+                (_userBase, 00005);
 
             bool checkSignalResult = false;
             StartCommunicationSignal signal
