bdd363ab3a72334d8446bb90ba40d9e0fe36369f
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2023-12-21 05:26:13 +0300

    add tests

---
+++ Tests/CreateConnectionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CreateConnectionTests
+    {
+        [TestMethod]
+        public void TestConnectAsyncMethod()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestDisonnectAsyncMethod()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestStopMethod()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestAcceptAsyncMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Tests/TestMovement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestMovement
+    {
+        [TestMethod]
+        public void TestPlayerMovement()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestConnectedPlayerMovement()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
